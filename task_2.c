3e7b74111f38f37718f3c9211247ab3cac36c609
Augustine Omenuwa <Elitetech7827@gmail.com>
2023-12-04 22:20:01 -0800

    Task 2

---
+++ task_2.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints an unsigned integer in binary format.
+ * @args: Arguments list.
+ *
+ * Return: The number of characters printed.
+ */
+
+int print_binary(va_list args)
+{
+	unsigned int num = va_arg(args, unsigned int);
+	int count = 0;
+	int binary[32];
+	int i = 0;
+
+	if (num == 0)
+	{
+		_putchar('0');
+		return 1;
+	}
+
+	while (num > 0)
+	{
+		binary[i] = num % 2;
+		num = num / 2;
+		i++;
+	}
+
+	while (--i >= 0)
+	{
+		_putchar(binary[i] + '0');
+		count++;
+	}
+
+	return count;
+}
